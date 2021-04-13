810c13f2262812452e1cb0fde287b47d24c60b35
Andi Aljabar <andi.aljabar30@gmail.com>
2021-04-14 02:06:04 +0700

    Fixing bug in models/M_Menu

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://[::1]/xXx/bengkel_app/';
+$config['base_url'] = 'http://localhost/PHP_EngineCI/';
 
 /*
 |--------------------------------------------------------------------------
